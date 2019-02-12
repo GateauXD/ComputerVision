e378656615f0d69e2163ce81475e60cb9852fffe
Dan <dantran2016@gmail.com>
2019-02-12 00:58:30 -0800

    Fixed the inverse rotation

--- Lab 2/lab2.m
+++ Lab 2/lab2.m
@@ -32,11 +32,11 @@ rotateB = zeros(300, 400, 3, 'uint8');
 degree = 60;
 for x1 = 1:400
     for y1 = 1:300
-        x2 =  round(cosd(degree)*(x1 - 200) - sind(degree)*(y1 -150)+ 200);
-        y2 =  round(sind(degree)*(x1 - 200) + cosd(degree)*(y1 -150) + 150);
+        x2 =  round(cosd(degree)*(x1 - 200) + sind(degree)*(y1 -150)+ 200);
+        y2 =  round(-sind(degree)*(x1 - 200) + cosd(degree)*(y1 -150) + 150);
        
         if y2 <= 300 && y2 > 0 && x2 <= 400 && x2 > 0
-            rotateB(y2, x2, :) = img1(y1, x1, :);
+            rotateB(y1, x1, :) = img1(y2, x2, :);
         end
     end
 end
