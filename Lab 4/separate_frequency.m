2c7c3c7731ec27e6c653715917656724827f9e52
GateauXDclear <dantran2016@gmail.com>
2019-03-04 22:45:32 -0800

    Computer vision lab 4 started

--- Lab 4/separate_frequency.m
+++ Lab 4/separate_frequency.m
@@ -6,7 +6,7 @@ function [low_pass_img, high_pass_img] = separate_frequency(img, ratio)
     
     
     %% shift the frequency map
-    
+    frequency_map_shifted = fftshift(frequency_map);
 
     %% compute low-frequency mask
     % create a rectangular mask whose:
@@ -16,13 +16,18 @@ function [low_pass_img, high_pass_img] = separate_frequency(img, ratio)
     
     
     %% separate low-frequency and high-frequency maps
-    
+    low_frequency_map_shifted = frequency_map_shifted .* mask;
+    high_frequency_map_shifted = frequency_map_shifted .* (1-mask);
     
     %% shift frequency maps back
-    
+    low_frequency_map = ifftshift(low_frequency_map_shifted);
+    high_frequency_map = ifftshift(high_frequency_map_shifted);
     
     %% apply Inverse FFT
-    low_pass_img = real(ifft2(frequency_map)); % change frequency_map to your low-frequency map
-    high_pass_img = real(ifft2(frequency_map)); % change frequency_map to your high-frequency map
+    low_pass_img = real(ifft2(low_frequency_map)); % change frequency_map to your low-frequency map
+    high_pass_img = real(ifft2(high_frequency_map)); % change frequency_map to your high-frequency map
     
+    %% Save the images
+    imwrite(low_pass_img, 'lena_low_pass.jpg');
+    imwrite(high_pass_img, 'lena_high_pass.jpg');
 end
