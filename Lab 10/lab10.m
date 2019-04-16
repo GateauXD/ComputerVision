43b988e19833ef8897dc6e8f6e1ae0dbeb72a252
Dan <dantran2016@gmail.com>
2019-04-16 15:00:43 -0700

    Finished Lab 10

--- Lab 10/lab10.m
+++ Lab 10/lab10.m
@@ -1,10 +1,10 @@
 %% Load input images
-img_dir = 'data/Army';
+img_dir = 'data/Urban';
 img1 = im2double(imread(fullfile(img_dir, 'frame10.png')));
 img2 = im2double(imread(fullfile(img_dir, 'frame11.png')));
 
 %% parameters
-window_size = 45;
+window_size = 90;
 k = 4; % number of iterations
 
 w = floor(window_size/2);
@@ -30,18 +30,23 @@ v_next = zeros(size(I1));
 for t = 1:k
     for i = 1 + shift : size(Ix_m, 1) - shift
         for j = 1 + shift : size(Ix_m, 2) - shift
-
             %% extract Ix, Iy, It from local window
-            
-            
+            IX = Ix_m(i-w:i+w,j-w:j+w,:);
+            IY = Iy_m(i-w:i+w,j-w:j+w,:);
+            i2 = i + v(i,j);
+            j2 = j + v(i,j);
+            IT = I1(floor(i-w):floor(i+w), floor(j-w):floor(j+w)) - I2(floor(i2-w):floor(i2+w), floor(j2-w):floor(j2+w));
             %% convert Ix, Iy, It to vectors
-
+            IX_v = IX(:);
+            IY_v = IY(:);
+            IT_v = IT(:);
             
             %% construct matrix A and vector b
-            
+            A = [IX_v, IY_v];
+            B = [-IT_v];
             
             %% solve A x = b
-            x = [0, 0]; % remove this line
+            x = pinv(A) * B;
             
             u_next(i, j) = x(1);
             v_next(i, j) = x(2);
